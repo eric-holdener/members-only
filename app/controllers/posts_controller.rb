dee9342187da97e6036a2fded310569043bb93cb
EHoldener <eholdener12@gmail.com>
2022-05-09 20:59:40 -0700

    signup page works, post new/create works, need to create the view form for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,21 +3,22 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
